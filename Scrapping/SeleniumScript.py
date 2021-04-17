4df34c3283c0f91743c4cc3df586d054753a9ca8
MuhammadAzmulHaq <decentazm101@outlook.com>
2021-04-17 09:31:27 +0500

    Upwork Script

--- Scrapping/SeleniumScript.py
+++ Scrapping/SeleniumScript.py
@@ -1,27 +1,34 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+from selenium.webdriver.chrome.options import Options
 
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://www.fastly.com/network-map')
-print(driver.title)
-# Locators
-time.sleep(2)
-# wait = WebDriverWait(driver, 10)
-# element = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='tachometer']/div[2]")))
+def fast():
+    options = Options()
+    options.headless = True
+    # Locators
+    Driver = webdriver.Chrome(options=options,
+                              executable_path=r'C:\Users\laptrade\PycharmProjects\Driver\chromedriver.exe')
+    # wait = WebDriverWait(driver, 10)
+    # element = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='tachometer']/div[2]")))
+    Driver.get('https://www.fastly.com/network-map')
+    print(Driver.title)
+    time.sleep(2)
+    meter = Driver.find_element_by_xpath("//*[@id='tachometer']/div[2]")
+    print("Requests per second", meter.text)
 
-meter = driver.find_element_by_xpath("//*[@id='tachometer']/div[2]")
-print("Requests per second", meter.text)
+    hit = Driver.find_element_by_css_selector("div[class='tach-stat hit-ratio flex-col'] div[class='tach-value']")
+    print("Cache hit ratio", hit.text)
 
-hit = driver.find_element_by_css_selector("div[class='tach-stat hit-ratio flex-col'] div[class='tach-value']")
-print("Cache hit ratio", hit.text)
+    time_hit = Driver.find_element_by_css_selector("div[class='tach-stat ttfb flex-col'] div[class='tach-value']")
+    print("Hit time", time_hit.text)
 
-time_hit = driver.find_element_by_css_selector("div[class='tach-stat ttfb flex-col'] div[class='tach-value']")
-print("Hit time", time_hit.text)
+    band = Driver.find_element_by_css_selector("div[class='tach-stat bandwidth flex-col'] div:nth-child(1)")
+    print("Bandwidth", band.text)
 
-band = driver.find_element_by_css_selector("div[class='tach-stat bandwidth flex-col'] div:nth-child(1)")
-print("Bandwidth", band.text)
+    Driver.close()
+    Driver.quit()
 
-driver.close()
-driver.quit()
+
+fast()
--- Scrapping/upwork.py
+++ Scrapping/upwork.py
@@ -1,21 +1,46 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.expected_conditions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
-Driver = webdriver.Chrome(ChromeDriverManager().install())
-Driver.get("https://www.upwork.com/ab/account-security/login")
-Driver.maximize_window()
-Login = Driver.find_element_by_id("login_username").send_keys("faizan.berlin@gmail.com")
-Continue_btn = Driver.find_element_by_id("login_password_continue").click()
-Driver.implicitly_wait(90)
-Ps_text = Driver.find_element_by_name("login[password]").click()
-Password = Driver.find_element_by_id("login_password").send_keys("reallyawesome123")
-Login_btn = Driver.find_element_by_id("login_control_continue").click()
-time.sleep(2)
-Driver.get("https://www.upwork.com/ab/jobs/search/?sort=recency")
-jobs = Driver.find_element_by_class_name("ng-binding")
-print(jobs.text)
+def jobs():
+    try:
+        options = Options()
+        options.headless = True
+        Driver = webdriver.Chrome(options=options,
+                                  executable_path=r'C:\Users\laptrade\PycharmProjects\Driver\chromedriver.exe')
+        Driver.get("https://www.upwork.com/ab/account-security/login")
+        print("Headless Chrome Initialized")
+        time.sleep(5)
+        Driver.save_screenshot("after.png")
+        Login = WebDriverWait(Driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='login_username']]"))).click()
+        Driver.save_screenshot("before.png")
+        # Login = Driver.find_element_by_css_selector("#login_username")
+        # Login.click()
+        Login.send_keys("faizan.berlin@gmail.com")
+        Continue_btn = Driver.find_element_by_id("login_password_continue")
+        Continue_btn.click()
+        Driver.implicitly_wait(90)
+        Ps_text = Driver.find_element_by_name("login[password]")
+        Ps_text.click()
+        Password = Driver.find_element_by_id("login_password")
+        Password.click()
+        Password.send_keys("reallyawesome123")
+        Login_btn = Driver.find_element_by_id("login_control_continue")
+        Login_btn.click()
+        time.sleep(2)
+        Driver.get("https://www.upwork.com/ab/jobs/search/?sort=recency")
+        jobs_count = Driver.find_element_by_class_name("ng-binding")
+        print(jobs_count.text)
+        Driver.close()
+        Driver.quit()
+    except:
+        raise NoSuchElementException("No Element found")
 
-Driver.close()
-Driver.quit()
+
+jobs()
